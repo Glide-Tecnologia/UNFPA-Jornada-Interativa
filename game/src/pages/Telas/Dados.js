acdb447a618ab7d62398dfa3e496ae8b90392dc1
unknown <andre@glide.com.br>
2023-12-12 15:44:17 -0300

    pagina com os selects

--- game/src/pages/Telas/Dados.js
+++ game/src/pages/Telas/Dados.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import 'react-simple-keyboard/build/css/index.css'
 import './Telas.css'
+import './Dados.css'
 import DataVaultComponent from '../../components/DataVault'
 import Configuration from '../../components/Configuration/Configuration'
 
@@ -17,12 +18,6 @@ function Cadastro () {
   const [fieldsChecked, setFieldsChecked] = useState(false)
 
   const navigate = useNavigate()
-
-  const inputRef = useRef(null)
-
-  useEffect(() => {
-    inputRef.current.focus()
-  }, [])
 
   const salvar = async () => {
     console.log('Tentando Salvar')
@@ -132,55 +127,99 @@ function Cadastro () {
       setTouchCount(0) // Reinicia a contagem após 5 toques
     }
   }, [touchCount])
+  // const [days, setDays] = useState([])
+
+  function buildDays(){
+    var days = [];
+    for (let i = 1; i <= 31; i++) {
+      days.push(<option key={i} value={i}>{i}</option>)
+    }
+    return days; 
+  }
+
+  function buildMonths(){
+    var months = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+    var optionsMonths=[]
+    months.forEach(element => {
+      optionsMonths.push(<option key={element} value={element}>{element}</option>)
+    });
+    return optionsMonths; 
+  }
+
+  function buildYears(){
+    var years = [];
+    for (let i = 1910; i <= 2023; i++) {
+      years.push(<option key={i} value={i}>{i}</option>)
+    }
+    return years; 
+  }
+
+  function buildGenders(){
+    var gender = ["Mulher trans","Mulher cis","Homem cis","Homem trans","Não-binarie","Prefiro não responder","Outro"];
+    var optionsGender=[]
+    gender.forEach(element => {
+      optionsGender.push(<option key={element} value={element}>{element}</option>)
+    });
+    return optionsGender; 
+  }
+
+  function buildRaces(){
+    var race = ["Preta", "Parda", "Branca", "Indígena", "Amarela"];
+    var optionsRace=[]
+    race.forEach(element => {
+      optionsRace.push(<option key={element} value={element}>{element}</option>)
+    });
+    return optionsRace; 
+  }
+
+  function buildStates(){
+    var states = ["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO","EX"]
+    var optionsState=[]
+    states.forEach(element => {
+      optionsState.push(<option key={element} value={element}>{element}</option>)
+    });
+    return optionsState; 
+  }
+
 
   return (
     <div className='cadastro'>
+      <img src="img/icpd-30.png" className='icpd-30 absolute' />
+
+      <div className='titulo-nome absolute'>Estamos quase acabando!</div>
+      
       <Configuration />
       {/* <DataVaultComponent /> */}
       <div className='btn-logs' onTouchStart={handleTouch}></div>
-      <div className='cadastro__titulo'>
-        Olá,
-        <br></br>
-        <br></br>
-        Somos a B3, a bolsa do Brasil!
-      </div>
-      <div className='cadastro__texto'>
-        Temos um ecossistema de empresas parceiras para entregar a melhor
-        solução para você.
-        <br></br>
-        <br></br>
-        Confira nossas soluções que trazem mais segurança, integração e
-        inteligência de dados para potencializar suas estratégias de negócio.
-      </div>
-      <div className='cadastro__termos'>
-        Estes dados serão utilizados para lhe enviar informativos sobre os
-        nossos serviços, além de utilizarmos para efeitos estatísticos. Ao
-        utilizar este formulário você concorda com a nossa Política. Podemos
-        compartilhar estes dados com outras marcas do mesmo Grupo para a mesma
-        finalidade. Caso deseje se desinscrever, você pode fazê-lo a partir de
-        algum e-mail já recebido (através da função descadastrar) assim
-        exercendo os seus direitos de usuário para retificação ou remoção dos
-        dados sensíveis.
-      </div>
       <div className='inputs'>
-        <input
-          ref={inputRef}
-          id='email'
-          autoComplete='off'
-          type='text'
-          value={getInputValue('email')}
-          onFocus={() => setInputName('email')}
-          placeholder={'Qual é seu email?'}
-          onChange={onChangeInput}
-          className={
-            (!inputs.email || !isEmailValid(inputs.email)) && erro
-              ? 'input--error'
-              : ''
-          }
-        />
+        <label className='lblBirthDate'>Data de Nascimento:</label>
+        <select className='select-date select-date__day'>
+          <option value="" disabled selected>Dia</option>
+          {buildDays()}
+        </select>
+        <select className='select-date select-date__month'>
+          <option value="" disabled selected>Mês</option>
+          {buildMonths()}
+        </select>
+        <select className='select-date select-date__year'>
+          <option value="" disabled selected>Ano</option>
+          {buildYears()}
+        </select>
+        <select className='select-gender'>
+          <option value="" disabled selected>Gênero</option>
+          {buildGenders()}
+        </select>
+        <select className='select-race'>
+          <option value="" disabled selected>Raça</option>
+          {buildRaces()}
+        </select>
+        <select className='select-state'>
+          <option value="" disabled selected>Estado de Residência</option>
+          {buildStates()}
+        </select>
       </div>
-      <div className='btn-salvar' onTouchStart={() => salvar()}>
-        ACEITO
+      <div className='btn-salvar-comentario' onTouchStart={() => salvar()}>
+        <img src="img/seta.svg"></img>
       </div>
     </div>
   )
